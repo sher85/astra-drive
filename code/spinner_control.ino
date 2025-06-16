1445efaffb7c50a11ede0b5e1c7872e2511b43c5
Mauricio Castro <32082005+sher85@users.noreply.github.com>
2025-06-15 20:24:33 -0500

    refactor: project can now handle decimals for ultra low rpms

--- code/spinner_control.ino
+++ code/spinner_control.ino
@@ -1,18 +1,19 @@
-#include <Stepper.h>
+#include <AccelStepper.h>
 
 // Constants
 const int stepsPerRevolution = 2048; // for 28BYJ-48 stepper motor
-const int motorSpeedRPM = 15;         // Desired RPM
+const int motorSpeedRPM = 5;         // Desired RPM
 const int buttonPin = 2;             // Button input pin
 
 // Create stepper instance (pin order: IN1, IN3, IN2, IN4)
-Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+AccelStepper myStepper(AccelStepper::HALF4WIRE, 8, 10, 9, 11);
 
 bool motorOn = false;
 int lastButtonState = HIGH;
 
 void setup() {
-  myStepper.setSpeed(motorSpeedRPM);
+  myStepper.setMaxSpeed(motorSpeedRPM * stepsPerRevolution / 60.0);
+  myStepper.setSpeed(motorSpeedRPM * stepsPerRevolution / 60.0);
   pinMode(buttonPin, INPUT_PULLUP); // Enable internal pull-up resistor
   Serial.begin(9600);
 }
@@ -30,7 +31,6 @@ void loop() {
   lastButtonState = buttonState;
 
   if (motorOn) {
-    myStepper.step(1); // Step 1 step at a time
-    // Timing is handled automatically by setSpeed()
+    myStepper.runSpeed();
   }
 }
