3337e8bbff7bcab2e1e5b7b97726c50acba4a86e
Mauricio Castro <32082005+sher85@users.noreply.github.com>
2025-06-15 21:13:00 -0500

    feat: now supports on-off LED

--- code/spinner_control.ino
+++ code/spinner_control.ino
@@ -4,6 +4,7 @@
 const int stepsPerRevolution = 2048; // for 28BYJ-48 stepper motor
 const int motorSpeedRPM = 5;         // Desired RPM
 const int buttonPin = 2;             // Button input pin
+const int ledPin = 3;                // LED control pin
 
 // Create stepper instance (pin order: IN1, IN3, IN2, IN4)
 AccelStepper myStepper(AccelStepper::HALF4WIRE, 8, 10, 9, 11);
@@ -14,7 +15,9 @@ int lastButtonState = HIGH;
 void setup() {
   myStepper.setMaxSpeed(motorSpeedRPM * stepsPerRevolution / 60.0);
   myStepper.setSpeed(motorSpeedRPM * stepsPerRevolution / 60.0);
-  pinMode(buttonPin, INPUT_PULLUP); // Enable internal pull-up resistor
+  pinMode(buttonPin, INPUT_PULLUP); // Button with internal pull-up
+  pinMode(ledPin, OUTPUT);          // Set LED pin as output
+  digitalWrite(ledPin, LOW);        // Ensure LED starts off
   Serial.begin(9600);
 }
 
@@ -25,6 +28,10 @@ void loop() {
   if (buttonState == LOW && lastButtonState == HIGH) {
     motorOn = !motorOn;
     Serial.println(motorOn ? "Motor ON" : "Motor OFF");
+
+    // LED matches motor state
+    digitalWrite(ledPin, motorOn ? HIGH : LOW);
+
     delay(250); // Debounce delay
   }
 
